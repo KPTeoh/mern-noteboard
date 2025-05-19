1e955c77f326005786e5a624915fd7de0c35c941
KPTeoh <khaipongteoh@gmail.com>
2025-05-19 21:53:17 +0800

    Create get API Route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js"
 
 const app = express();
 
-app.get("/api/try", (req,res)=>{
-    res.send("You just built your first haha API");
-});
+app.use("/api/notes", notesRoutes)
+
+// app.get("/api/try", (req,res)=>{
+//     res.send("You just built your first haha API");
+// });
 
 app.listen(5001, () => {
     console.log("Server is running on port: 5001");
